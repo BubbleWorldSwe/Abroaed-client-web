116315a818b8457cc1fad477fe106432dbc40e0d
devyansh <devyansh.sehgal@gmail.com>
2024-11-13 00:13:44 +0530

    added new pages

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
-import Home from "./Components/Home";
-import BlogPage from "./Components/BlogPage";
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import Home from './Components/Home';
+import BlogPage from './Components/BlogPage';
+import LayoutPage from './pages/destinations/layout';
+import TestPrepLayoutPage from './pages/TestPrep/Layout';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<BlogPage />} />
+        <Route path="/destinations" element={<LayoutPage />} />
+        <Route path="/testprep" element={<TestPrepLayoutPage />} />
       </Routes>
     </BrowserRouter>
   );
