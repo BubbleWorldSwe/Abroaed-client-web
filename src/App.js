2bbad23d6ebf727bc6c4fd6603a69c78bf3539b8
devyansh <devyansh.sehgal@gmail.com>
2024-11-22 00:06:35 +0530

    unwanted files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { UserContextProvider } from './contexts/userContext';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/routes.js';
 import { AppContextProvider } from './contexts/appContext.js';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import AppRoutes from '../website/src/routes/index.jsx';
 
 const App = () => {
   return (
