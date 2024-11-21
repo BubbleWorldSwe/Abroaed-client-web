82b52f66d65b42fa59720ec8d5aa84d6ff44d99a
devyansh <devyansh.sehgal@gmail.com>
2024-11-22 00:09:27 +0530

    delete slice dashboard

--- website/src/store/adminStore.js
+++ website/src/store/adminStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from "../slices/userSlice"
-import dashboardSlice from "../slices/dashboardSlice"
 
 
 const adminStore = configureStore({
     reducer: {
         users: userSlice,
-        dashboard: dashboardSlice,
     },
 });
 
