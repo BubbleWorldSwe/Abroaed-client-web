3bb0eb0adb51669579e5e2b1c68172acea8fd3be
devyansh <devyansh.sehgal@gmail.com>
2024-12-05 15:44:24 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require("tailwindcss/defaultTheme");
-const svgToDataUri = require("mini-svg-data-uri");
-const colors = require("tailwindcss/colors");
-const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
-
+import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette';
+import svgToDataUri from 'mini-svg-data-uri';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   plugins: [
     require('flowbite/plugin')({
@@ -17,51 +13,53 @@ export default {
     }),
     addVariablesForColors,
     function ({ matchUtilities, theme }) {
-      matchUtilities({
-        "bg-grid": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-          )}")`,
-        }),
-        "bg-grid-small": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-          )}")`,
-        }),
-        "bg-dot": (value) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-          )}")`,
-        }),
-      }, { values: flattenColorPalette(theme("backgroundColor")), type: "color" });
+      matchUtilities(
+        {
+          'bg-grid': (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          'bg-grid-small': (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          'bg-dot': (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      );
     },
   ],
   darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
-        'soft-yellow-gradient': 'linear-gradient(135deg, #fffde7, #fff9c4, #ffecb3)',
+        'soft-yellow-gradient':
+          'linear-gradient(135deg, #fffde7, #fff9c4, #ffecb3)',
       },
       colors: {
         primary: {
-          "50": "#fffdea",
-          "100": "#fff3c4",
-          "200": "#ffe78e",
-          "300": "#ffdb5d",
-          "400": "#ffcd2b",
-          "500": "#ffc300",
-          "600": "#e6ad00",
-          "700": "#b38900",
-          "800": "#806400",
-          "900": "#4d3f00",
-          "950": "#2a2200"
+          '50': '#fffdea',
+          '100': '#fff3c4',
+          '200': '#ffe78e',
+          '300': '#ffdb5d',
+          '400': '#ffcd2b',
+          '500': '#ffc300',
+          '600': '#e6ad00',
+          '700': '#b38900',
+          '800': '#806400',
+          '900': '#4d3f00',
+          '950': '#2a2200',
         },
-      }
-      ,
+      },
       animation: {
-        scroll:
-          "scroll var(--animation-duration) linear infinite",
-        spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll: 'scroll var(--animation-duration) linear infinite',
+        spotlight: 'spotlight 2s ease .75s 1 forwards',
       },
       keyframes: {
         scroll: {
@@ -73,29 +71,51 @@ export default {
           '100%': { transform: 'translateX(0%)' },
         },
         spotlight: {
-          "0%": {
+          '0%': {
             opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
+            transform: 'translate(-72%, -62%) scale(0.5)',
           },
-          "100%": {
+          '100%': {
             opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
+            transform: 'translate(-50%,-40%) scale(1)',
           },
         },
       },
     },
     fontFamily: {
-      'body': [
-        'Inter', 'ui-sans-serif', 'system-ui', '-apple-system',
-        'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue',
-        'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji',
-        'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'
+      body: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
       ],
-      'sans': [
-        'Inter', 'ui-sans-serif', 'system-ui', '-apple-system',
-        'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue',
-        'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji',
-        'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'
+      sans: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
       ],
       inter: ['Inter', 'sans-serif'],
     },
@@ -103,11 +123,12 @@ export default {
 };
 
 function addVariablesForColors({ addBase, theme }) {
-  const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
 
   addBase({
-    ":root": newVars,
+    ':root': newVars,
   });
 }
