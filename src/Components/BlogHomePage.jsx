f66a948899f1b06b7de37cc57dd271268ee28095
devyansh <devyansh.sehgal@gmail.com>
2024-12-21 21:34:47 +0530

    blogs home page cards

--- src/Components/BlogHomePage.jsx
+++ src/Components/BlogHomePage.jsx
@@ -3,6 +3,163 @@ import React from "react";
 function BlogHomePage() {
   return (
     <div>
+      <div class="py-8 lg:py-24 bg-gray-50 dark:bg-gray-800 antialiased mx-auto ">
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 ">
+          <article class="p-4 mx-auto max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-900 dark:border-gray-700">
+            <a href="#">
+              <img
+                class="mb-5 rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops.png"
+                alt="office laptop working"
+              />
+            </a>
+            <div class="flex items-center mb-3 space-x-2">
+              <img
+                class="w-8 h-8 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                alt="Jese Leos avatar"
+              />
+              <div class="font-medium dark:text-white">
+                <div>Jese Leos</div>
+                <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Aug 15, 2021 · 16 min read
+                </div>
+              </div>
+            </div>
+            <h3 class="mb-2 text-xl font-bold tracking-tight text-gray-900 lg:text-2xl dark:text-white">
+              <a href="#">Our first office</a>
+            </h3>
+            <p class="mb-3 text-gray-500 dark:text-gray-400">
+              Over the past year, Volosoft has undergone many changes! After
+              months of preparation and some hard work, we moved to our new
+              office.
+            </p>
+            <a
+              href="#"
+              class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 hover:no-underline"
+            >
+              Read more{" "}
+              <svg
+                class="mt-px ml-1 w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </article>
+          <article class="hidden p-4 mx-auto max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-900 dark:border-gray-700 sm:block">
+            <a href="#">
+              <img
+                class="mb-5 rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/google-hq.png"
+                alt="google hq"
+              />
+            </a>
+            <div class="flex items-center mb-3 space-x-2">
+              <img
+                class="w-8 h-8 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png"
+                alt="Roberta Casas avatar"
+              />
+              <div class="font-medium dark:text-white">
+                <div>Roberta Casas</div>
+                <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Aug 15, 2021 · 16 min read
+                </div>
+              </div>
+            </div>
+            <h3 class="mb-2 text-xl font-bold tracking-tight text-gray-900 lg:text-2xl dark:text-white">
+              <a href="#">We partnered up with Google</a>
+            </h3>
+            <p class="mb-3 text-gray-500 dark:text-gray-400">
+              Over the past year, Volosoft has undergone many changes! After
+              months of preparation and some hard work, we moved to our new
+              office.
+            </p>
+            <a
+              href="#"
+              class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 hover:no-underline"
+            >
+              Read more{" "}
+              <svg
+                class="mt-px ml-1 w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </article>
+          <article class="hidden p-4 mx-auto max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-900 dark:border-gray-700 xl:block">
+            <a href="#">
+              <img
+                class="mb-5 rounded-lg"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops-2.png"
+                alt="office laptop working"
+              />
+            </a>
+            <div class="flex items-center mb-3 space-x-2">
+              <img
+                class="w-8 h-8 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
+                alt="Sofia McGuire avatar"
+              />
+              <div class="font-medium dark:text-white">
+                <div>Sofia McGuire</div>
+                <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Aug 15, 2021 · 16 min read
+                </div>
+              </div>
+            </div>
+            <h3 class="mb-2 text-xl font-bold tracking-tight text-gray-900 lg:text-2xl dark:text-white">
+              <a href="#">Our first project with React</a>
+            </h3>
+            <p class="mb-3 text-gray-500 dark:text-gray-400">
+              Over the past year, Volosoft has undergone many changes! After
+              months of preparation and some hard work, we moved to our new
+              office.
+            </p>
+            <a
+              href="#"
+              class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 hover:no-underline"
+            >
+              Read more{" "}
+              <svg
+                class="mt-px ml-1 w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </article>
+        </div>
+      </div>
       <aside
         aria-label="Related articles"
         class="py-8 lg:py-24 bg-gray-50 dark:bg-gray-800 antialiased"
