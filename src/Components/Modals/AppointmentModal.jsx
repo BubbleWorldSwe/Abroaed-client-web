1669373c6f63c217cb6d401a7c030ef6f9e7d896
Shahid Raza <shahidraza02qwert@gmail.com>
2025-01-27 23:20:44 +0530

    add lead flow

--- src/Components/Modals/AppointmentModal.jsx
+++ src/Components/Modals/AppointmentModal.jsx
@@ -24,8 +24,15 @@ const AppointmentModal = ({ leadId, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-      <div className="bg-white rounded-lg p-6 w-96">
+      <div className="bg-white font-rethink dark:bg-gray-900 rounded-lg shadow-lg p-6 w-full max-w-max relative">
+        <button
+          className="absolute w-10 h-10 top-2 right-2 text-gray-600 hover:text-gray-900 text-2xl"
+          onClick={onClose}
+        >
+          &times;
+        </button>
         <h2 className="text-xl font-semibold mb-4">Schedule Appointment</h2>
+
         <div className="space-y-4">
           <div>
             <label className="block text-gray-700">Date</label>
--- src/Components/Modals/AssignTeamMemberModal.jsx
+++ src/Components/Modals/AssignTeamMemberModal.jsx
@@ -87,17 +87,28 @@ const AssignTeamModal = ({ leadId, leadName, team = {}, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-      <div className="bg-white rounded-lg p-6 w-96">
-        <h2 className="text-xl font-semibold mb-4">Assign Team Members</h2>
-
-        <div className="space-y-4">
+      <div className="bg-white font-rethink dark:bg-gray-900 rounded-lg shadow-lg p-6 w-full max-w-max relative">
+        <button
+          className="absolute w-10 h-10 top-2 right-2 text-gray-600 hover:text-gray-900 text-2xl"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        <h2 className="text-xl font-semibold mb-4">Assign Team Member</h2>
+        <h5 className="text-lg font-medium">
+          Assigned Members
+        </h5>
+        <p className="text-sm">
+          No members assigned. Assign below
+        </p>
+        <div className="grid mt-3 grid-cols-1 gap-4 lg:grid-cols-2">
           <div>
-            <label className="block text-gray-700">Counsellor</label>
+            <label className="block text-gray-700 font-medium mb-1">Counsellor</label>
             <select
               name="counsellor"
               value={selectedTeam.counsellor}
               onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
+              className="w-full px-3 py-2 border-none bg-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
             >
               <option value="">Select Counsellor</option>
               {getMembersByRole("Counsellor").map((member) => (
@@ -109,12 +120,12 @@ const AssignTeamModal = ({ leadId, leadName, team = {}, onClose }) => {
           </div>
 
           <div>
-            <label className="block text-gray-700">Backend Manager</label>
+            <label className="block text-gray-700 font-medium mb-1">Backend Manager</label>
             <select
               name="backendManager"
               value={selectedTeam.backendManager}
               onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
+              className="w-full px-3 py-2 border-none bg-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
             >
               <option value="">Select Backend Manager</option>
               {getMembersByRole("Backend Manager").map((member) => (
@@ -125,23 +136,9 @@ const AssignTeamModal = ({ leadId, leadName, team = {}, onClose }) => {
             </select>
           </div>
 
-          <div>
-            <label className="block text-gray-700">Mentor</label>
-            <select
-              name="mentor"
-              value={selectedTeam.mentor}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-            >
-              <option value="">Select Mentor</option>
-              {getMembersByRole("Mentor").map((member) => (
-                <option key={member.id} value={member.id}>
-                  {member.name}
-                </option>
-              ))}
-            </select>
-          </div>
+
         </div>
+
 
         <div className="flex justify-end space-x-2 mt-4">
           <button
