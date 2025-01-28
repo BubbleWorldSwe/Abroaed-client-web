b1398461f509e1ecb8912706aa4419babee73795
Shahid Raza <shahidraza02qwert@gmail.com>
2025-01-28 16:02:48 +0530

    add asignment feature in leads

--- src/Components/Modals/ConfirmModal.jsx
+++ src/Components/Modals/ConfirmModal.jsx
@@ -14,7 +14,7 @@ const ConfirmModal = ({
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
                     <div className="bg-white font-rethink dark:bg-gray-900 rounded-lg shadow-lg p-6 w-full max-w-max relative">
                         <button
-                            className="absolute w-10 h-10 top-2 right-2 text-gray-600 hover:text-gray-900 text-2xl"
+                            className="absolute w-10 h-10 top-1 right-1 text-gray-600 hover:text-gray-900 text-2xl"
                             onClick={onClose}
                         >
                             &times;
@@ -27,10 +27,10 @@ const ConfirmModal = ({
 
                                 <button
                                     type="submit"
-                                    className="bg-blue-600 text-white px-4 py-2 rounded-md"
+                                    className="bg-blue-600 w-full text-white px-4 py-2 rounded-md"
                                     onClick={onClose}
                                 >
-                                    Add
+                                    Done
                                 </button>
                             </div>
                         </div>
