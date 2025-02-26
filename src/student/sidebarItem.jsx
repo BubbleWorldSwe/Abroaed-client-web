6c3ba1728a4bb9d40f882347831ea60c41470127
Juhi Kukreja <1611juhi@gmail.com>
2025-02-26 18:28:01 +0530

    remove simplify things in lang and test preps

--- src/student/sidebarItem.jsx
+++ src/student/sidebarItem.jsx
@@ -2,17 +2,19 @@
 import { useNavigate } from "react-router-dom";
 
 const SidebarItem = ({ path, label, Icon, isActive }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <button
-            onClick={() => navigate(path)}
-            className={`flex items-center px-4 py-3 rounded-md cursor-pointer ${isActive(path) ? "text-black font-bold" : "text-gray-400"} hover:text-black hover:font-bold transition`}
-        >
-            <Icon className="w-5 h-5" />
-            <span className="ml-3 whitespace-nowrap">{label}</span>
-        </button>
-    );
+  return (
+    <button
+      onClick={() => navigate(path)}
+      className={`flex items-center px-4 py-3 rounded-md cursor-pointer ${
+        isActive(path) ? "text-black font-bold" : "text-gray-400"
+      } hover:text-black hover:font-bold transition`}
+    >
+      <Icon className="w-5 h-5" />
+      <span className="ml-3 whitespace-nowrap">{label}</span>
+    </button>
+  );
 };
 
 export default SidebarItem;
