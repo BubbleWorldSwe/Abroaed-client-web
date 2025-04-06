50dd8054f7329d7f6395d85b3c8d8dd652b96ba7
Shahid Raza <shahidraza02qwert@gmail.com>
2025-04-06 19:50:45 +0530

    add button

--- src/website/home/webpages/homePage.jsx
+++ src/website/home/webpages/homePage.jsx
@@ -30,6 +30,7 @@ import Blogs from "../../comman/components/blogs";
 import Testimonials from "../../comman/components/testimonials";
 // import vectorDownNose from "../../../assets/vectorDownNose.png";
 import vectorleftNose from "../../../assets/vectorleftNose.png";
+import vectoreLeftFlat from "../../../assets/vectoreLeftFlat.png";
 import vectorRightNoseCurve from "../../../assets/vectorRightNoseCurve.png";
 import vectorBelow from "../../../assets/vectorBelow.png";
 import { addLeadRequest } from "../../../redux/actions/leadsActions";
@@ -52,22 +53,32 @@ function HomeLayout() {
       <Header />
       <HomeHeroSection />
       <HomeSlidingImg />
-      <HomeKpiMatrixSection
+      <div className="relative ">
+ <HomeKpiMatrixSection
         title={"Why ABROAED?"}
         header={"What Sets Us Apart?"}
         subtitle={
           "ABROAED distinguishes itself through its highly personalized and student-centric approach to study abroad consulting. We offer end-to-end support, customizing every step—from university selection and application strategy to visa assistance and pre-departure preparation—to align with each student’s unique goals and aspirations. Leveraging data-driven insights and a proven track record of success, we ensure students target the best-fit programs that align with their academic and career ambitions. At ABROAED, we’re not just helping you move across borders; we’re helping you create a future filled with endless possibilities."
         }
       />
+     <div className="absolute top-0 left-0 -z-20">
+          <img
+            className="rounded-lg max-w-full"
+            src={vectoreLeftFlat}
+            alt="Counselling session"
+          />
+        </div>
+      </div>
+     
       <HomePathwaySection />
       <div className="relative ">
         <HomeServicesSection />
-        <div className="absolute -top-10 right-0 -z-20">
-          {/* <img
+        <div className="absolute top-0 right-0 -z-20">
+          <img
             className="rounded-lg max-w-full"
             src={vectorleftNose}
             alt="Counselling session"
-          /> */}
+          />
         </div>
       </div>
       <HomePromoSection
@@ -78,21 +89,21 @@ function HomeLayout() {
       <Testimonials />
       <div className="relative ">
         <div className="absolute -top-10 left-0 z-0">
-          {/* <img
+          <img
             className="rounded-lg max-w-full "
             src={vectorRightNoseCurve}
             alt="Counselling session"
-          /> */}
+          />
         </div>
         <HomeDownloadApp />
       </div>
       <div className="relative ">
         <div className="absolute top-28 right-0 z-0">
-          {/* <img
+          <img
             className="rounded-lg max-w-full "
             src={vectorBelow}
             alt="Counselling session"
-          /> */}
+          />
         </div>
         <Blogs />
       </div>
