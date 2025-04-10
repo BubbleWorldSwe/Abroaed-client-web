f2328805c7f95d4e05dc25480c6666a8a26187b5
Shahid Raza <shahidraza02qwert@gmail.com>
2025-04-10 22:34:04 +0530

    modify modals

---
+++ src/website/comman/modals/servicesNavModal.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/prop-types */
+
+import { ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
+
+
+const ServicesNavModal = ({ handleMouseEnter,
+    handleMouseLeave, }) => {
+    const items =
+        [
+            { title: "Finance", href: "/finance" },
+            { title: "Accommodation", href: "/accomodation" },
+            { title: "Home Counselling", href: "/homeCounselling" },
+        ]
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    return (
+        <div
+            className="absolute left-0 top-full w-max py-[5px]  z-50"
+            onMouseEnter={() => handleMouseEnter("services")}
+            onMouseLeave={() => setTimeout(handleMouseLeave, 200)}
+        >
+            <ul className="space-1 grid grid-cols-1 shadow-lg w-[15vw]  rounded-b-lg mt-2 bg-white" >
+                {items?.map((data, index) => (
+                    <li
+                        key={index}
+                        className="flex  items-center justify-between  text-sm text-gray-600 font-semibold hover:text-gray-900   border-b border-gray-200  px-3 py-1 hover:bg-gray-100 rounded-lg transition-all"
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        <div className="flex  w-full justify-between items-center ">
+                            <a
+                                href={data.href}
+                                className="block px-4 py-1 "
+                            >
+                                {data?.title}
+                            </a>
+                            <ChevronRightIcon
+                                className={`w-5 h-5   ${hoveredIndex === index
+                                    ? " opacity-100"
+                                    : "opacity-0"
+                                    }`}
+                            />
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ServicesNavModal
