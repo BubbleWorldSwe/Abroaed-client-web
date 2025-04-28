de754a055b2f35b831c73182466df3f2191be9bb
Shahid Raza <shahidraza02qwert@gmail.com>
2025-04-28 08:13:00 +0530

    test

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -60,7 +60,7 @@ import ForgetPasswordSuccessfull from "../website/authentication/forgetPassword/
 import Login from "../website/authentication/login";
 import AddBlog from "../admin/blogs/screens/addBlog";
 import EditBlog from "../admin/blogs/screens/editBlog";
-import ComingSoonPage from "../layout/comingSoonPage";
+import ComingSoonPage from "../website/comingSoon/comingSoonPage";
 
 const AppRoutes = () => {
   const allRoutes = [
