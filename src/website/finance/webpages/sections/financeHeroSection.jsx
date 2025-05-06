27bcfd6a694ee5c818388b598a7d9b6e3964ee19
Shahid Raza <shahidraza02qwert@gmail.com>
2025-05-06 10:01:40 +0530

    remove para

--- src/website/finance/webpages/sections/financeHeroSection.jsx
+++ src/website/finance/webpages/sections/financeHeroSection.jsx
@@ -9,9 +9,6 @@ function FinanceHeroSection() {
       <h1 className={heroStyle.header}>
         Smart Financial Planning for Your Study Abroad Success
       </h1>
-      {/* <p className={heroStyle.text}>
-                Access global elite education at top-tier universities with personalized guidance
-            </p> */}
     </HeroTextComponent>
   );
 }
