db77059c4c7a6502ecf098506acf0e0ffe666db0
Juhi Kukreja <1611juhi@gmail.com>
2025-06-06 00:01:30 +0530

    collges list screen

--- src/website/college/webpages/sections/collegeCard.jsx
+++ src/website/college/webpages/sections/collegeCard.jsx
@@ -33,49 +33,47 @@ const CollegeCard = ({
           src={
             logoImage
               ? `${IMAGE_BASE_URL}/${logoImage?.ImageUrl}`
-              : IMAGES.noCollege
+              : IMAGES.noLogo
           }
           alt={item.name}
         />
       </div>
 
       {/* Content Section */}
-      <div className="ml-4 flex flex-col flex-grow">
-        {/* Header */}
-        <div className="flex justify-between items-start">
-          <h5 className="text-[18px] line-clamp-1 mb-2 font-semibold tracking-tight text-gray-primary dark:text-white">
+      <div className="flex items-start justify-between ml-3 w-full">
+        <div>
+          <h5 className="text-[18px] line-clamp-2 font-semibold tracking-tight text-gray-primary dark:text-white">
             {item.name}
           </h5>
 
-          {isLoggedInStudent && (
-            <button
-              onClick={(e) => {
-                e.preventDefault(); // Prevent link redirect when clicking bookmark
-                isSaved
-                  ? removeFromSavedPreferences(savedItem._id)
-                  : addToSavedPreferences("colleges", item?._id);
-              }}
-            >
-              <Bookmark
-                className={`w-5 h-5 ml-2 ${
-                  isSaved ? "fill-black text-black" : "text-gray-500"
-                }`}
-              />
-            </button>
-          )}
+          <div className="flex items-center">
+            <img
+              className="w-[14px] h-[14px] object-contain mr-1"
+              src={locationIcon}
+              alt="location"
+            />
+            <p className="font-semibold text-[13px] text-gray-500 dark:text-gray-400 line-clamp-1">
+              {item?.stateId?.name}, {item?.destinationId?.countryId?.name}
+            </p>
+          </div>
         </div>
 
-        {/* Location */}
-        <div className="mb-1 flex items-center">
-          <img
-            className="w-[14px] h-[14px] object-contain mr-1"
-            src={locationIcon}
-            alt="location"
-          />
-          <p className="font-semibold text-[13px] text-gray-500 dark:text-gray-400 line-clamp-1">
-            {item?.stateId?.name}, {item?.destinationId?.countryId?.name}
-          </p>
-        </div>
+        {isLoggedInStudent && (
+          <button
+            onClick={(e) => {
+              e.preventDefault(); // Prevent link redirect when clicking bookmark
+              isSaved
+                ? removeFromSavedPreferences(savedItem._id)
+                : addToSavedPreferences("colleges", item?._id);
+            }}
+          >
+            <Bookmark
+              className={`w-5 h-5 ml-2 ${
+                isSaved ? "fill-black text-black" : "text-gray-500"
+              }`}
+            />
+          </button>
+        )}
       </div>
     </a>
   );
--- src/website/comman/contexts/heroStyle.js
+++ src/website/comman/contexts/heroStyle.js
@@ -1,4 +1,5 @@
 export const heroStyle = {
-    text: "text-[24px] md:text-[32px] font-normal opacity-90    text-white",
-    header: "mb-3  text-[42px]  font-medium  leading-none text-white md:text-[75px]"
-}
+  text: "text-[24px] md:text-[32px] font-normal opacity-90 text-white",
+  header:
+    "mb-3  text-[42px]  font-medium  leading-none text-white md:text-[75px]",
+};
